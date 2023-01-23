12e63ff4e818a2cd69561623359459c43a0c531d
Lademir Bechara <becharalademir76@gmail.com>
2023-01-23 00:37:13 -0300

    Terceiro passo

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,28 +1,17 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-   private String titulo;
-   private String descrição;
-   private  int cargaHoraria;
+public class Curso extends Conteudo{
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescrição() {
-        return descrição;
-    }
-
-    public void setDescrição(String descrição) {
-        this.descrição = descrição;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -35,8 +24,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descrição='" + descrição + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
